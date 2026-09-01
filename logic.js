df4aaa8c90270025f5e02313e39cf5b7875e8693
9Justin9 <justinleon0412@gmail.com>
2026-09-01 02:48:47 +0200

    Exercise class and Bench Press exercise

--- logic.js
+++ logic.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// Classes
+class Exercise {
+    constructor(name, sets, reps, weight) {
+        this.name = name
+        this.sets = sets
+        this.reps = reps
+        this.weight = weight
+    }
+    volume () {
+        return(this.sets * this.reps * this.weight + " kg")
+    }
+};
+
+// Exercises
+const benchPress = new Exercise("Bench Press", 3, 8, 85);
+console.log(benchPress.volume());
+console.log(benchPress);
